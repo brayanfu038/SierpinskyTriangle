e91a69006ea40078b18ffb44db6779307f8e0845
Brayan daniel fuquene sandoval <brayan.fuquene01@uptc.edu.co>
2023-09-03 13:08:12 -0500

    se mejoro el metodo para dibujar el triangulo

--- src/model/SierpinskyTriangle.java
+++ src/model/SierpinskyTriangle.java
@@ -34,29 +34,25 @@ public class SierpinskyTriangle extends JFrame{
 	}
 	
 	public void drawTriangle(int x1,int x2, int x3,int y1,int y2, int y3,Graphics g ,int n) {
-		if(n==0) {
+		if(n>0) {
+			int aux_X1 = (x1+x2)/2;int aux_X2 = (x1+x3)/2;int aux_X3 =(x2+x3)/2;
+			int aux_Y1 =(y1+y2)/2;int aux_Y2 =(y1+y3)/2; int aux_Y3=(y2+y3)/2; 
 			
-		}else {	
-		int aux_X1 = (x1+x2)/2;int aux_X2 = (x1+x3)/2;int aux_X3 =(x2+x3)/2;
-		int aux_Y1 =(y1+y2)/2;int aux_Y2 =(y1+y3)/2; int aux_Y3=(y2+y3)/2; 
-		
-		int[] xPoints={aux_X1,aux_X2,aux_X3};
-		int[] yPoints={aux_Y1,aux_Y2,aux_Y3};
-		
-		g.setColor(Color.WHITE);
-		g.fillPolygon(xPoints,yPoints,3);
-		
-		// Triangulo Inferior Izquierdo
-		this.drawTriangle(x1, aux_X1,aux_X2,y1,aux_Y1,y1, g, n-1);
-		
-		//Triangulo Inferior Derecho
-		this.drawTriangle(aux_X2, aux_X3,x3,y1,aux_Y3,y1, g, n-1);
-		
-		//Triangulo Superior
-		this.drawTriangle(aux_X1, aux_X2,aux_X3,aux_Y1,y2,aux_Y3,g, n-1);
+			int[] xPoints={aux_X1,aux_X2,aux_X3};
+			int[] yPoints={aux_Y1,aux_Y2,aux_Y3};
 			
-		}
-		
+			g.setColor(Color.WHITE);
+			g.fillPolygon(xPoints,yPoints,3);
+			
+			// Triangulo Inferior Izquierdo
+			this.drawTriangle(x1, aux_X1,aux_X2,y1,aux_Y1,y1, g, n-1);
+			
+			//Triangulo Inferior Derecho
+			this.drawTriangle(aux_X2, aux_X3,x3,y1,aux_Y3,y1, g, n-1);
+			
+			//Triangulo Superior
+			this.drawTriangle(aux_X1, aux_X2,aux_X3,aux_Y1,y2,aux_Y3,g, n-1);
+		}		
 	}
 	public static int readInt(String message) {
 		return Integer.parseInt(JOptionPane.showInputDialog(message));
